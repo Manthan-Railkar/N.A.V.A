ac21add866f18d2987f4d8ea3fcaaa739e3c2426
Manthan-Railkar <railkarmanthan@gmail.com>
2025-08-17 00:06:01 +0530

    Update weather_predicter.py

--- weather_predicter.py
+++ weather_predicter.py
@@ -5,21 +5,18 @@ def weather_report(response):
     w_r = data['weather'][0]["main"]
     desc = data['weather'][0]["description"]
     temp = data['main']['temp']
-    print(w_r)
-    print(desc)
-    print(temp)
+    return w_r,desc,temp 
 
 def get_api_response(city,unit,language):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=c9451e57162ef769db79f5a47916bd93&units={unit}&lang={language}'
     res = requests.get(url)
     return res 
 
-def get_data(): 
-    city = input("Enter the city name to get the weather report :") 
-    unit = input("Enter the units in which you want the report: ")
-    language = input("Enter your preferred language code as per: ")
+def get_data(city): 
+    city =city 
+    unit ='me'
+    language ='en'
 
     response = get_api_response(city,unit,language) 
     weather_report(response) 
 
-get_data()
